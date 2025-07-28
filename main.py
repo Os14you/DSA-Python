16f56a72db017737bf9544c09eb27d487cddbbd3
Osama <os14you.manage@gmail.com>
2025-07-28 14:47:13 +0300

    Adding test function in the module and optamize the main function

--- main.py
+++ main.py
@@ -1,34 +1,7 @@
-from scattered.LinkedList import LinkedList
-
-def test_linked_list():
-    ll = LinkedList()
-    ll.insert_front(1)
-    ll.insert_front(2)
-    ll.insert_front(3)
-    ll.insert_end(4)
-
-    assert ll.get(0) == 3
-    assert ll.get(1) == 2
-    assert ll.get(2) == 1
-    assert ll.get(3) == 4
-
-    ll.delete_front()
-    assert ll.get(0) == 2
-    assert ll.get(1) == 1
-    assert ll.get(2) == 4
-
-    ll.delete_end()
-    assert ll.get(0) == 2
-    assert ll.get(1) == 1
-
-    ll.delete(1)
-    assert ll.get(0) == 2
-
-    print("All tests passed!")
-
+from scattered.LinkedList import test_linked_list
 
 def main():
     test_linked_list()
-
+    
 if __name__ == "__main__":
     main()
--- scattered/LinkedList.py
+++ scattered/LinkedList.py
@@ -172,3 +172,32 @@ class LinkedList(Generic[T]):
         
         # Use the iterator to build a string representation
         return f"LinkedList({ ' -> '.join(map(str, self)) })"
+
+def test_linked_list():
+    ll = LinkedList()
+    ll.insert_front(1)
+    ll.insert_front(2)
+    ll.insert_front(3)
+    ll.insert_end(4)
+
+    assert ll.get(0) == 3
+    assert ll.get(1) == 2
+    assert ll.get(2) == 1
+    assert ll.get(3) == 4
+
+    ll.delete_front()
+    assert ll.get(0) == 2
+    assert ll.get(1) == 1
+    assert ll.get(2) == 4
+
+    ll.delete_end()
+    assert ll.get(0) == 2
+    assert ll.get(1) == 1
+
+    ll.delete(1)
+    assert ll.get(0) == 2
+
+    print("All tests passed!")
+
+if __name__ == "__main__":
+    test_linked_list()
