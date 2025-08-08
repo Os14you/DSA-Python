3c4846f8c54aec3a783efcf70f7c630b98840aa1
Osama <os14you.manage@gmail.com>
2025-08-08 14:27:22 +0300

    Fixing the main.py file

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from scattered import test_linked_list, test_stack, test_queue, test_binary_tree
+from scattered import test_linked_list, test_binary_tree, test_BST
+from consecutive import test_queue, test_stack
 
 def main():
     test_linked_list()
     test_stack()
     test_queue()
     test_binary_tree()
+    test_BST()
 
 if __name__ == "__main__":
     main()
